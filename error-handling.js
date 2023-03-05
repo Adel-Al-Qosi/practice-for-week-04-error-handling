949c225d577d58e0e874a95e5af593e9bf39c547
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-03-05 21:15:03 +0300

    finish practice

--- error-handling.js
+++ error-handling.js
@@ -7,14 +7,30 @@ function sum(array) {
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+try {
+  let res = sum(null);
+  console.log(res);
+} catch (error) {
+  console.error(error.message);
+}
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
 // Your code here
+function sayName(name) {
+  if (typeof name === "string") {
+    console.log(name);
+  } else {
+    throw TypeError("Invalid name! Must be a string");
+  }
+}
+
+try {
+  sayName("Alex");
+  sayName(1);
+} catch (error) {
+  console.error(error.message);
+}
 
 // 3.
 function greet(greeting) {
@@ -23,4 +39,10 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try {
+  greet(false);
+} catch {
+  console.log("Hello World!");
 }
